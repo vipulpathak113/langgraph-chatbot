087571435c0d3173c8c918281ab6c162a3adba52
Yoda_Vipul <vipulpathak113@gmail.com>
2025-07-30 01:23:18 +0530

    adding memory checkpoint to chatbot

--- chatbot.py
+++ chatbot.py
@@ -3,6 +3,7 @@ from typing import Annotated, TypedDict
 from langchain_core.messages import BaseMessage, HumanMessage
 from langchain_openai import ChatOpenAI
 from langgraph.graph.message import add_messages
+from langgraph.checkpoint.memory import MemorySaver
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
@@ -22,20 +23,25 @@ def run_chatbot(state: ChatbotState) -> str:
     return {"messages": messages + [response]}
 
 
+checkpointer = MemorySaver()
 graph = StateGraph(ChatbotState)
 graph.add_node("chatbot", run_chatbot)
 
 graph.add_edge(START, "chatbot")
 graph.add_edge("chatbot", END)
 
-chatbot = graph.compile()
+chatbot = graph.compile(checkpointer=checkpointer)
 
 
 # adding conversation to chatbot
 
+thread_id = "1"
 while True:
     user_input = input("You: ")
     if user_input.lower() in ["exit", "quit"]:
         break
-    response = chatbot.invoke({"messages": [HumanMessage(content=user_input)]})
+    config = {"configurable": {"thread_id": thread_id}}
+    response = chatbot.invoke(
+        {"messages": [HumanMessage(content=user_input)]}, config=config
+    )
     print("Chatbot:", response["messages"][-1].content)
