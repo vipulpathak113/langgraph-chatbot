e05be33f44eb35fabb6cf28b4dc289d4a2c79ac0
Yoda_Vipul <vipulpathak113@gmail.com>
2025-07-30 00:52:45 +0530

    conversational chatbot

--- chatbot.py
+++ chatbot.py
@@ -8,24 +8,34 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
+
 class ChatbotState(TypedDict):
     messages: Annotated[list[BaseMessage], add_messages]
-    
 
-llm = ChatOpenAI();
+
+llm = ChatOpenAI()
+
 
 def run_chatbot(state: ChatbotState) -> str:
-    messages = state['messages']
+    messages = state["messages"]
     response = llm.invoke(messages)
-    return { 'messages': messages + [response] }
+    return {"messages": messages + [response]}
 
 
-graph= StateGraph(ChatbotState)
-graph.add_node('chatbot',run_chatbot)
+graph = StateGraph(ChatbotState)
+graph.add_node("chatbot", run_chatbot)
 
-graph.add_edge(START, 'chatbot')
-graph.add_edge('chatbot', END)
+graph.add_edge(START, "chatbot")
+graph.add_edge("chatbot", END)
 
-chatbot= graph.compile();
+chatbot = graph.compile()
 
-initial_message={"messages": [HumanMessage(content="What is the capital of india?")]}
+
+# adding conversation to chatbot
+
+while True:
+    user_input = input("You: ")
+    if user_input.lower() in ["exit", "quit"]:
+        break
+    response = chatbot.invoke({"messages": [HumanMessage(content=user_input)]})
+    print("Chatbot:", response["messages"][-1].content)
